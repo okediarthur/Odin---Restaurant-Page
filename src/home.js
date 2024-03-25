8cfbdadad259dec0ae062e9bee4cbf18eeb263eb
okediarthur <okediarthur@gmail.com>
2024-03-25 14:18:50 +0300

    Home.js adjustments

--- src/home.js
+++ src/home.js
@@ -1,5 +1,16 @@
 export function generateHomeContent(){
-    const homeContent = document.createElement('p');
-    homeContent.textContent = "Welcome to Tony's Steakhouse.";
-    return homeContent;
+    // const homeContent = document.createElement('p');
+    // homeContent.textContent = "Welcome to Tony's Steakhouse.";
+    // return homeContent;
+    const div = document.createElement('div');
+    div.classList.add('home-content');
+
+    const title = document.createElement('h1');
+    title.textContent = "Gastrol Pub";
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = "Indulge in a symphony of flavors at Gastrol Pub, where every dish is a culinary masterpiece and every sip a celebration. Experience the perfect fusion of gastronomic delights and pub charm, where hearty meals meet artisanal brews in an ambiance that's as lively as it is inviting. Join us for a dining experience that's not just a meal, but a journey through taste sensations!";
+
+    const image1 = document.createElement('img');
+    image1.src = './images/'
 }
